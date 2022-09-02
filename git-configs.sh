97197ca3cae7b780690e506dc28eb4b46387e12a
luiggy macias <ferrinluiggy@gmail.com>
2022-09-02 13:12:06 +0000

    rrr

---
+++ git-configs.sh
@@ -0,0 +1,16 @@
+ #!/bin/bash
+# set an alias for git
+# alias for status
+git config --global alias.s status
+# alias for add
+git config --global alias.a "add ."
+# alias for commit
+git config --global alias.c "commit -m"
+# alias for push
+git config --global alias.p "push"
+# alias for pull
+git config --global alias.pl "pull"
+
+# change the user name and email
+git config --global user.name "luiggy macias"
+git config --global user.email "ferrinluiggy@gmail.com"
