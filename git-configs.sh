504228a859b2ff51ebe9a964e2f320944295efbd
luiggy macias <ferrinluiggy@gmail.com>
2022-09-02 14:05:55 +0000

    sssdd

--- git-configs.sh
+++ git-configs.sh
@@ -14,3 +14,11 @@ git config --global alias.pl "pull"
 # change the user name and email
 git config --global user.name "luiggy macias"
 git config --global user.email "ferrinluiggy@gmail.com"
+git config credential.username "macluiggy"
+git config --global credential.useHttpPath true
+# git config --global -e
+
+git config --global credential.helper store
+
+# para clonar con token
+#  git clone https://macluiggy:<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>@github.com/ordering-app.git
